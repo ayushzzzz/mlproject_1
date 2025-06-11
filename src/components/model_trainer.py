b1ce4f6f8168729ac86cba8b8ecf2a1c46f86a95
ayushzzzz <ayush.ghawana@gmail.com>
2025-06-11 19:46:23 +0530

    Model Trainer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,70 @@
+from sklearn.model_selection import train_test_split
+import sys, os
+import numpy as np
+from src.logger import logging
+from src.exception import CustomException
+from dataclasses import dataclass
+from src.utils import evaluate_model, save_object
+
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+
+        try:
+            logging.info("Splitting of data")
+
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1],
+            )
+
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Linear Regression": LinearRegression(),
+            }
+
+            model_report: dict = evaluate_model(
+                X_train, y_train, X_test, y_test, models=models
+            )
+
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = models[best_model_name]
+            print(best_model)
+
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+            logging.info("best model found now")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model,
+            )
+
+            predicted = best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+
+            return r2_square
+        except Exception as e:
+            raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -20,3 +20,23 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+
+        report = {}
+        for model_name, model in models.items():
+
+            model.fit(X_train, y_train)
+
+            y_pred = model.predict(X_test)
+
+            test_model_score = r2_score(y_test, y_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
